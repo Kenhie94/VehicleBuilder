451d441bd6647e660a50bf35fa913747e27dd77a
Kenjy Jap <kenhiebusiness@gmail.com>
2024-09-30 15:08:42 -0700

    added truck and motorbike to createVehicle

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -58,15 +58,19 @@ class Cli {
           name: "vehicleType",
           message: "Select a vehicle type",
           // TODO: Update the choices array to include Truck and Motorbike
-          choices: ["Car"],
+          choices: ["Car", "Truck", "Motorbike"],
         },
       ])
       .then((answers) => {
         if (answers.vehicleType === "Car") {
           // create a car
           this.createCar();
+          // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
+        } else if (answers.vehicleType === "Truck") {
+          this.createTruck();
+        } else if (answers.vehicleType === "Motorbike") {
+          this.createMotorbike();
         }
-        // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
       });
   }
 
@@ -245,21 +249,12 @@ class Cli {
       ])
       .then((answers) => {
         // TODO: Use the answers object to pass the required properties to the Motorbike constructor
-        const motorbike = new Motorbike(
-          Cli.generateVin(),
-          answers.color,
-          answers.make,
-          answers.model,
-          parseInt(answers.year),
-          parseInt(answers.weight),
-          parseInt(answers.topSpeed),
-          [
-            new Wheel(parseInt(answers.frontWheelDiameter), answers.frontWheelBrand,),
-            new Wheel(parseInt(answers.rearWheelDiameter),answers.rearWheelBrand)
-          ]
-        );
+        const motorbike = new Motorbike(Cli.generateVin(), answers.color, answers.make, answers.model, parseInt(answers.year), parseInt(answers.weight), parseInt(answers.topSpeed), [
+          new Wheel(parseInt(answers.frontWheelDiameter), answers.frontWheelBrand),
+          new Wheel(parseInt(answers.rearWheelDiameter), answers.rearWheelBrand),
+        ]);
         // TODO: push the motorbike to the vehicles array
-        this.vehicles.push(motorbike)
+        this.vehicles.push(motorbike);
         // TODO: set the selectedVehicleVin to the vin of the motorbike
         this.selectedVehicleVin = motorbike.vin;
         // TODO: perform actions on the motorbike
